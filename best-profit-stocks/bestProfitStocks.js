c35b4f9088006b772c56733cce008a6a1c2c1ed7
cen19 <wilsoncen@gmail.com>
2018-04-12 15:53:04 -0700

    added tests for bestProfitStocks + cleanup

--- best-profit-stocks/bestProfitStocks.js
+++ best-profit-stocks/bestProfitStocks.js
@@ -1,8 +1,5 @@
-var stockPricesYesterday = [10, 7, 5, 8, 11, 9];
-// returns 6 (buying for $5 and selling for $11)
-
-let getMaxProfit = function(stockPrices) {
-  let lowestPrice = stockPricesYesterday[0];
+const getMaxProfit = (stockPrices) => {
+  let lowestPrice = stockPrices[0];
   let maxProfit = 0;
 
   for (let i = 0; i < stockPrices.length; i++) {
@@ -16,7 +13,7 @@ let getMaxProfit = function(stockPrices) {
   return maxProfit;
 };
 
-let findHighest = function(stockPrices, startIndex) {
+const findHighest = (stockPrices, startIndex) => {
   let highest = stockPrices[startIndex];
   for (let i = startIndex + 1; i < stockPrices.length; i++) {
     if (stockPrices[i] > highest) {
@@ -26,4 +23,6 @@ let findHighest = function(stockPrices, startIndex) {
   return highest;
 };
 
-console.log(getMaxProfit(stockPricesYesterday));
+module.exports = {
+  getMaxProfit
+};
