691faec503774f2d327cbf4d9ac2e06910b449eb
cen19 <wilsoncen@gmail.com>
2018-04-11 18:21:06 -0700

    fixed spacing

--- arrayPairSum/arrayPairSum.js
+++ arrayPairSum/arrayPairSum.js
@@ -1,9 +1,10 @@
 const arrayPairSum = (nums) => {
-  let sorted = nums.sort((a, b) => a - b);
+  const sorted = nums.sort((a, b) => a - b);
   let sum = 0;
 
   for (var i = 0; i < sorted.length; i += 2) {
     sum += sorted[i];
   }
+  
   return sum;
 };
