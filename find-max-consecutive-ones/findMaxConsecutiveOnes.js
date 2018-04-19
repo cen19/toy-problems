4460dc149154be835d73ed2a39f25cfa0f09f3fd
cen19 <wilsoncen@gmail.com>
2018-04-19 02:25:42 -0700

    refactored

--- find-max-consecutive-ones/findMaxConsecutiveOnes.js
+++ find-max-consecutive-ones/findMaxConsecutiveOnes.js
@@ -3,18 +3,15 @@ let test = [1, 1, 0, 1, 1, 1];
 const findMaxConsecutiveOnes = function(nums) {
   let highest = 0;
   let current = 0;
-  for (var i = 0; i < nums.length; i++) {
-    if (nums[i] === 0) {
-      if (current > highest) {
-        highest = current;
-      }
+
+  nums.forEach(num => {
+    if (num === 0) {
+      if (current > highest) { highest = current; }
       current = 0;
     } else {
-      console.log('here at', i);
       current++;
-      console.log(current);
     }
-  }
+  });
   return highest > current ? highest : current;
 };
 
