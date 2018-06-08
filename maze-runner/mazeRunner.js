1ab2caab9f43c98e589f1bbd4e8602218f3e5b8c
cen19 <wilsoncen@gmail.com>
2018-06-08 01:10:30 -0700

    refactored slightly

--- maze-runner/mazeRunner.js
+++ maze-runner/mazeRunner.js
@@ -1,52 +1,36 @@
 const mazeRunner = function(maze, directions) {
-  // define the way pieces move throughout the array of arrays when given a direction
-  let currentLocation = '';
-  maze.forEach((row, i) => {
-    row.forEach((el, j) => {
-      if (el === 2) {
-        currentLocation = [i, j];
-      }
-    });
-  });
+  let currY = maze.findIndex(array => array.includes(2));
+  let currX = maze[currY].findIndex(el => el === 2);
 
-  const isSafeToGo = function(coords) {
-    // takes in coords
-    let min = Math.min(...coords);
-    let max = Math.max(...coords);
-    if (min < 0) { return 'Dead'; }
-    if (max > maze.length - 1) { return 'Dead'; }
-    const spot = maze[coords[0]][coords[1]];
-
-    if (spot === 1) {
+  const isSafeToGo = function(x, y) {
+    if (maze[y] === undefined || maze[y][x] === undefined || maze[y][x] === 1) {
       return 'Dead';
-    } else if (spot === 3) {
+    }
+
+    if (maze[y][x] === 3) {
       return 'Finish';
     }
 
     return true;
   };
 
-  let futureSpot = [];
-
   for (let i = 0; i < directions.length; i++) {
-    futureSpot = [...currentLocation];
     if (directions[i] === 'N') {
-      futureSpot[0] = futureSpot[0] - 1;
+      currY--;
     } else if (directions[i] === 'S') {
-      futureSpot[0] = futureSpot[0] + 1;
+      currY++;
     } else if (directions[i] === 'W') {
-      futureSpot[1] = futureSpot[1] - 1;
+      currX--;
     } else if (directions[i] === 'E') {
-      futureSpot[1] = futureSpot[1] + 1;
+      currX++;
     }
 
-    let spot = isSafeToGo(futureSpot);
-    if (spot !== true) {
-      return spot;
+    let status = isSafeToGo(currX, currY);
+    if (status !== true) {
+      return status;
     }
-    currentLocation = futureSpot;
   }
-
+  // return lost of we ran out of moves and not dead
   return 'Lost';
 };
 
