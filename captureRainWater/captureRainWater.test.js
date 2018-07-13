6a43132302bda14ccb271220b2c6e4a4f62c054e
cen19 <wilsoncen@gmail.com>
2018-07-13 00:34:31 -0700

    isPeak check working

--- captureRainWater/captureRainWater.test.js
+++ captureRainWater/captureRainWater.test.js
@@ -5,3 +5,7 @@ let test1 = [0, 1, 0];
 test('expeected to be peak', () => {
   expect(isPeak(test1, 1)).toBe(true);
 });
+
+test('expected false', () => {
+  expect(isPeak([1, 0], 0)).toBe(true);
+});
--- captureRainWater/captureRainWater_v2.js
+++ captureRainWater/captureRainWater_v2.js
@@ -1,13 +1,21 @@
 const trap = function(array) {
   // find first peak
+  let peak1 = 0;
+  let peak2 = null;
+
+  // for (let i = 0; i < array.length; i++) {
+  //   if ()
+  // }
 };
 
 const isPeak = function(array, index) {
-  return array[index] > array[index - 1] && array[index] > array[index + 1];
+  const leftOfIndex = array[index - 1] || 0;
+  const rightOfIndex = array[index + 1] || 0;
+  return (array[index] > leftOfIndex) && (array[index] > rightOfIndex);
 };
 
 let testArray = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
-
+console.log(isPeak([1, 0], 0));
 module.exports = {
   isPeak,
   trap
